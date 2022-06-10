5fe1f93971f87cc85c6ce918227c3fb9f6e9ed58
lcamposesp <lcamposesp@ucreativa.com>
2022-06-09 18:26:20 -0600

    moved app to use app = Flask(name)

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask,jsonify
 from api import scraper_nacion
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 SECRET_KEY = '1393459340023492'
-application.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
 
-@application.route('/')
+@app.route('/')
 def hello_world():  
         return('<h1>API Sencilla de Flask que hace un crawl de noticias de Costa Rica </h1>')
-@application.route('/deportes',methods=['GET'])
+@app.route('/deportes',methods=['GET'])
 def noticias_deportes():
     response =scraper_nacion.scraping_nacion_deportes()
     return response
-@application.route('/el-pais', methods=['GET'])
+@app.route('/el-pais', methods=['GET'])
 def noticias_el_pais():
     response = scraper_nacion.scraping_nacion_pais()
     return response 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
