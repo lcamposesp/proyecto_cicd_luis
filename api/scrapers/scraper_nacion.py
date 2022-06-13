fba66e6828cd9e0bad9e6d6bf140cae37d2c59f3
Luis <lcamposesp@ucreativa.com>
2022-06-13 14:14:12 -0600

    decoupling of logic for extra content
    logic for the extra content needs to be decoupled from previous method since the return is a simple list

--- api/scrapers/scraper_nacion.py
+++ api/scrapers/scraper_nacion.py
@@ -1,5 +1,6 @@
 from cmath import log
 import json
+from operator import add
 import requests
 from bs4 import BeautifulSoup
 
@@ -88,16 +89,23 @@ def scraping_nacion_deportes_for_index():
     #This is the method that will return the relevant titles and URLs to the front end that gets displayed in home
     dict_titles = {}
     dict_url = {}
+    dict_content = dict()
     r = requests.get('https://www.nacion.com/puro-deporte/')
     soup = BeautifulSoup(r.content, 'html.parser')
     s = soup.find('div', class_='results-list-container')
     content = s.find_all('a',href=True,title=True)
+    added_content = s.find_all('p')
     #Adding items from the content scraping
     key=1
     for item in content:
         dict_titles[key] = item['title']
         dict_url[key] = item['href']
         key += 1
+    key_more_content = 1
+    for item in added_content:
+        dict_content[key_more_content] = item.text
+        key_more_content+=1
+    print(dict_content)
     # Cleanup of duplicate results
     # This currently works for the titles being returned correctly but not the actual links, which are WIP   
     temp = []
@@ -114,10 +122,14 @@ def scraping_nacion_deportes_for_index():
             res_url[key] = val
     set_titles = list()
     set_url = list()
+    set_more_content = list()
     for key,val in res_titles.items():
         set_titles.append(val)
     for key,val in res_url.items():
         set_url.append(val)
+    for key,val in dict_content.items():
+        set_more_content.append(val)
+    print(set_more_content)
     # Uncomment the lines below for debugging more the assigment of titles to urls
     # Content to render is using a zip method, which combines two lists to a single dictionary creating a key,val association between the values of each 
     # print(set_titles)
@@ -288,3 +300,56 @@ def scraping_nacion_economia_for_index():
     # print(set_url)
     content_to_render = dict(zip(set_titles,set_url))
     return content_to_render
+
+def more_content_deportes():
+    # THIS NEEDS A LITTLE BIT OF CLEANING SINCE NOT ALL LINES ARE USED OR EVEN NEED TO BE CALLED. MOSTLY LINES THAT DO NOT FOCUS ON THE EXTRA CONTENT ADDED
+    #This is the method that will return the relevant titles and URLs to the front end that gets displayed in home
+    dict_titles = {}
+    dict_url = {}
+    dict_content = dict()
+    r = requests.get('https://www.nacion.com/puro-deporte/')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    s = soup.find('div', class_='results-list-container')
+    content = s.find_all('a',href=True,title=True)
+    added_content = s.find_all('p')
+    #Adding items from the content scraping
+    key=1
+    for item in content:
+        dict_titles[key] = item['title']
+        dict_url[key] = item['href']
+        key += 1
+    key_more_content = 1
+    for item in added_content:
+        dict_content[key_more_content] = item.text
+        key_more_content+=1
+    print(dict_content)
+    # Cleanup of duplicate results
+    # This currently works for the titles being returned correctly but not the actual links, which are WIP   
+    temp = []
+    res_titles = dict()
+    for key,val in sorted(dict_titles.items()):
+        if val not in temp:
+            temp.append(val)
+            res_titles[key] = val
+    temp_url = []
+    res_url = dict()
+    for key,val in sorted(dict_url.items()):
+        if val not in temp_url:
+            temp_url.append(val)
+            res_url[key] = val
+    set_titles = list()
+    set_url = list()
+    set_more_content = list()
+    for key,val in res_titles.items():
+        set_titles.append(val)
+    for key,val in res_url.items():
+        set_url.append(val)
+    for key,val in dict_content.items():
+        set_more_content.append(val)
+    print(set_more_content)
+    # Uncomment the lines below for debugging more the assigment of titles to urls
+    # Content to render is using a zip method, which combines two lists to a single dictionary creating a key,val association between the values of each 
+    # print(set_titles)
+    # print(set_url)
+    content_to_render = dict(zip(set_titles,set_url))
+    return set_more_content
